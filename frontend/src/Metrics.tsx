7282f9a78d3ad6900422030b201ff2f5902c6edf
mrglm <mrglm@protonmail.com>
2024-11-04 13:48:09 +0100

    feat: add an option to select either words or mistakes for the end
    This commit adds a Setting type, and a selectedSetting context.
    This context is passed to a SettingsSelector component.
    Once the state in the context is updtated, the corresponding setting is updated in the App file.

--- frontend/src/Metrics.tsx
+++ frontend/src/Metrics.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import SettingsContext from "./SelectedSettingContext";
 
 type MetricsProps = { nbMistakes: number; wpm: number };
 
 const Metrics = ({ nbMistakes, wpm }: MetricsProps): React.JSX.Element => {
+  const context = useContext(SettingsContext);
+  if (!context) {
+    throw new Error("SelectedSettingContext is null");
+  }
+  const selectedSetting = context.selectedSetting;
+
   return (
     <h2>
-      Mistakes: {nbMistakes} | WPM: {wpm}
+      Mistakes: {nbMistakes}
+      {selectedSetting.settingName === "Mistakes" ? ` / ${selectedSetting.settingValue}` : ""} | WPM: {wpm}
     </h2>
   );
 };
