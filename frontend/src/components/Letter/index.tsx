a34814a38d550bfb22660d39aa78dd572ff67d22
mrglm <mrglm@protonmail.com>
2024-11-19 09:39:28 +0100

    refactor: move the typing logic to the WordsContainer components

--- frontend/src/components/Letter/index.tsx
+++ frontend/src/components/Letter/index.tsx
@@ -3,20 +3,20 @@ import React, { useEffect, useRef } from "react";
 type LetterProps = {
   challengeChar: string;
   typedChar: string;
-  incrementNbMistakes: () => void;
+  updateNbMistakes: () => void;
 };
 
-const Letter = ({ challengeChar, typedChar, incrementNbMistakes }: LetterProps): React.JSX.Element => {
+const Letter = ({ challengeChar, typedChar, updateNbMistakes }: LetterProps): React.JSX.Element => {
   const status = !typedChar ? "" : !challengeChar ? "extra" : challengeChar === typedChar ? "correct" : "incorrect";
 
   const isMistakeRecorded = useRef(false);
 
   useEffect(() => {
     if ((status === "extra" || status === "incorrect") && !isMistakeRecorded.current) {
-      incrementNbMistakes();
+      updateNbMistakes();
       isMistakeRecorded.current = true;
     }
-  }, [incrementNbMistakes, status]);
+  }, [updateNbMistakes, status]);
 
   return (
     <span
